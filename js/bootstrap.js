0c84e124a505c950fafa14dea4dbd34cd35a2d82
Harry Parton <hparton@hotmail.co.uk>
2014-12-11 11:44:12 +0000

    Move scripts from index.html to bootstrap.js

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,3 +1,33 @@
-$(document).ready(function(){
+$(document).ready(function() {
+	$(':submit').on('click', function(event) {
+		event.preventDefault();
+
+		var $button = $(this),
+		$form = $button.parents('form');
+
+		var opts = $.extend({}, $button.data(), {
+			token: function(result) {
+				$form.append($('<input>').attr({ type: 'hidden', name: 'stripeToken', value: result.id })).submit();
+			}
+		});
+
+		StripeCheckout.open(opts);
+	});
+
+	var feed = new Instafeed({
+		get: 'tagged',
+		tagName: 'hotsmokybastard',
+		clientId: '9d06bacabe4d4e8ba04a40abb7f0e03f',
+		limit: 8, 
+		after: function () {
+		  var instagrams = $("#instafeed > a");
+		  for (var i = 6; i < instagrams.length; i += 1) {
+		    instagrams[i].remove();
+		  }
+		},
+		resolution: 'standard_resolution'
+	});
+	feed.run();
 
 });
+
